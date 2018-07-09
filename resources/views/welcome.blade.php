16d5b41781291e13afe8ee7fa582324ee7dee912
taizan0624 <taizan0624@gmail.com>
2018-07-09 05:37:28 +0000

    taiyama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <div class="cover">
         <div class="cover-inner">
             <div class="cover-contents">
-                <h1>素敵な本と出会う場所</h1>
+                <h1>Explore the World</h1>
                 @if (!Auth::check())
                     <a href="{{ route('signup.get') }}" class="btn btn-success btn-lg">KARIBONを始める</a>
                 @endif
