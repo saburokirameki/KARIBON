006aa8984bc8f555b0a918968e4d52b03cf47ea0
Austin Z <ec2-user@ip-172-31-14-59.us-east-2.compute.internal>
2018-07-09 03:34:09 +0000

    is numeric

--- resources/views/books/books.blade.php
+++ resources/views/books/books.blade.php
@@ -1,27 +1,29 @@
 @if ($books)
-    <div class="row">
+<div class="row">
         @foreach ($books as $book)
-            <div class="book">
-                <div class="col-md-3 col-sm-4 col-xs-12">
-                    <div class="panel panel-default">
-                        <div class="panel-heading text-center">
-                            <img src="{{ $book->image_url }}" alt="" class="">
-                        </div>
-                        <div class="panel-body">
-                            @if ($book->id)
-                                <p class="book-title"><a href="{{ route('books.show', $book->id) }}">{{ $book->name }}</a></p>
-                            @else
-                                <p class="book-title">{{ $book->name }}</p>
-                            @endif
-                            <div class="buttons text-center">
-                                @if (Auth::check())
-                                    @include('books.have_button', ['book' => $book])
+            @if (is_numeric($book->code))
+                <div class="book">
+                    <div class="col-md-3 col-sm-4 col-xs-12">
+                        <div class="panel panel-default">
+                            <div class="panel-heading text-center">
+                                <img src="{{ $book->image_url }}" alt="" class="">
+                            </div>
+                            <div class="panel-body">
+                                @if ($book->id)
+                                    <p class="book-title"><a href="{{ route('books.show', $book->id) }}">{{ $book->name }}</a></p>
+                                @else
+                                    <p class="book-title">{{ $book->name }}</p>
                                 @endif
+                                <div class="buttons text-center">
+                                    @if (Auth::check())
+                                        @include('books.have_button', ['book' => $book])
+                                    @endif
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            @endif
         @endforeach
     </div>
 @endif
